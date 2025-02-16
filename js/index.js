75c6c22e17e96e8ac6e180d97195c4099095d2d3
akash772000 <iamakashchowdhury9547@gmail.com>
2025-02-16 14:21:25 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function showSidebar()
+{
+    const sidebar=document.querySelector('.slidebar')
+    sidebar.style.display='flex'
+}
+function hidesidebar(){
+     const sidebar=document.querySelector('.slidebar')
+    sidebar.style.display='none'
+
+}
